ed9fd6ff56b4db864553db85be8b79cccb7411c6
cynthia muemi <muemiprecious@gmail>
2025-04-28 03:03:41 +0300

    sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    "lisk-sepolia": {
+      url: process.env.LISK_SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
